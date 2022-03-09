91d4a32e52578c26215c12ede570a657cb64f30a
Stephen Lee <stephenabcba@gmail.com>
2022-03-09 09:48:05 -0800

    selection sort

---
+++ morning-algo/mar9-2022.js
@@ -0,0 +1,42 @@
+/* 
+    https://visualgo.net/en/sorting
+    Selection sort works by iterating through the list, finding the minimum
+    value, and moving it to the beginning of the list. Then, ignoring the first
+    position, which is now sorted, iterate through the list again to find the
+    next minimum value and move it to index 1. This continues until all values
+    are sorted.
+    Unstable sort.
+    Time Complexity
+        - Best: O(n^2) quadratic.
+        - Average: O(n^2) quadratic.
+        - Worst: O(n^2) quadratic.
+    
+    Space: O(1) constant.
+    Selection sort is one of the slower sorts.
+        - ideal for: pagination, where page 1 displays 10 records for example,
+        you run selection sort for 10 iterations only to display the first 10
+        sorted items...
+*/
+const myArr = [3, 2, 9, 5, 1, 4, 8]
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    // original array is modified, returning is optional
+    return arr; // by now, everything is sorted!
+}
+
+selectionSort(myArr);
+console.log(myArr);
