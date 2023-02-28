0bda9054b0a217ea10b14e4daa5dbc1f3875db37
Stephen Lee <stephenabcba@gmail.com>
2023-02-28 14:51:03 -0800

    traversal assigning keys

---
+++ python/find_duplicate_subtrees.py
@@ -0,0 +1,72 @@
+# leetcode problem # 652. Find Duplicate Subtrees
+
+"""
+Given the root of a binary tree, return all duplicate subtrees.
+
+For each kind of duplicate subtrees, you only need to return the root node of any one of them.
+
+Two trees are duplicate if they have the same structure with the same node values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,null,2,4,null,null,4]
+Output: [[2,4],[4]]
+Example 2:
+
+
+Input: root = [2,1,1]
+Output: [[1]]
+Example 3:
+
+
+Input: root = [2,2,2,3,null,3,null]
+Output: [[2,3],[3]]
+ 
+
+Constraints:
+
+The number of the nodes in the tree will be in the range [1, 5000]
+-200 <= Node.val <= 200
+"""
+
+
+"""
+Solution by leetcode: id subtrees
+
+Assign each subtree structure with an unique id; subtrees with the same structure
+and values have the same ids
+To identify the subtrees, group the nodes in tuples in the structure (left component, node, right component)
+
+Runtime: O(N)
+Space: O(N)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def findDuplicateSubtrees(self, root):
+        def traverse(node):
+            if not node:
+                return 0
+            triplet = (traverse(node.left), node.val, traverse(node.right))
+            if triplet not in triplet_to_id:
+                triplet_to_id[triplet] = len(triplet_to_id) + 1
+            id = triplet_to_id[triplet]
+            cnt[id] += 1
+            if cnt[id] == 2:
+                res.append(node)
+            return id
+        triplet_to_id = dict()
+        cnt = collections.defaultdict(int)
+        res = []
+        traverse(root)
+        return res
