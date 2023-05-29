34df3f77eb0edb166a97cda717bbe42a942fb51c
Stephen Lee <stephenabcba@gmail.com>
2023-05-29 14:39:44 -0700

    list solution

---
+++ python/design_parking_system.py
@@ -0,0 +1,68 @@
+# leetcode problem # 1603. Design Parking System
+
+"""
+Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+Implement the ParkingSystem class:
+
+ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small, which are represented by 1, 2, and 3 respectively. A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+ 
+
+Example 1:
+
+Input
+["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+[[1, 1, 0], [1], [2], [3], [1]]
+Output
+[null, true, true, false, false]
+
+Explanation
+ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+parkingSystem.addCar(1); // return true because there is 1 available slot for a big car
+parkingSystem.addCar(2); // return true because there is 1 available slot for a medium car
+parkingSystem.addCar(3); // return false because there is no available slot for a small car
+parkingSystem.addCar(1); // return false because there is no available slot for a big car. It is already occupied.
+ 
+
+Constraints:
+
+0 <= big, medium, small <= 1000
+carType is 1, 2, or 3
+At most 1000 calls will be made to addCar
+"""
+
+"""
+My solution: save the spots as an array
+
+The most basic solution is to save the available spaces for each size in
+3 separate variables, updating them when cars come in
+
+By saving the spaces in a list, the logic can use list indexing to access
+the avaiable spaces and reducing redundant code
+
+
+Runtime:
+Init: O(1), runtime takes constant time
+addCar(): O(1), runtime takes constant time
+-> Overall: O(N) where N is the number of times addCat() is called
+Space:
+O(1), the program only save 3 integers (the number of spots available for each size)
+"""
+
+
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.spots = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.spots[carType - 1] > 0:
+            self.spots[carType - 1] -= 1
+            return True
+        return False
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
