2292fe4b6a02ff7b723310a80ef982c84f9d211a
Stephen Lee <stephenabcba@gmail.com>
2024-07-22 09:56:51 -0700

    dictionary solution

---
+++ python/sort_the_people.py
@@ -0,0 +1,57 @@
+# leetcode problem # 2418. Sort the People
+
+"""
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+
+ 
+
+Example 1:
+
+Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+Output: ["Mary","Emma","John"]
+Explanation: Mary is the tallest, followed by Emma and John.
+Example 2:
+
+Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+Output: ["Bob","Alice","Bob"]
+Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+
+Constraints:
+n == names.length == heights.length
+1 <= n <= 10^3
+1 <= names[i].length <= 20
+1 <= heights[i] <= 10^5
+names[i] consists of lower and upper case English letters.
+All the values of heights are distinct.
+"""
+
+"""
+My solution: Map to dictionary
+
+Idea: Connect the heights to the names, then sort the heights
+- after sorting, rearrange the people based on the connection made earlier
+
+Runtime: O(N * logN) where N is the number of people
+Space: O(N)
+"""
+
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        namedHeights = {}
+
+        n = len(names)
+
+        for idx in range(n):
+            namedHeights[heights[idx]] = names[idx]
+
+        heights.sort(reverse=True)
+
+        for idx in range(n):
+            names[idx] = namedHeights[heights[idx]]
+
+        return names
